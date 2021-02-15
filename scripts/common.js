99948b4cda6e49d7f65a29d1f2dedc5e77b4f19c
Sehbaz Rafik <sehbazm@gmail.com>
2021-02-15 07:19:29 +0000

    sign in and sign up functionality added

---
+++ scripts/common.js
@@ -0,0 +1,14 @@
+var signinModal = document.getElementById("signinModal");
+function opensigninModal() {
+    signinModal.style.display = "block";
+}
+var signupModal = document.getElementById("signupModal");
+function opensignupModal() {
+    signupModal.style.display = "block";
+  
+}
+
+function closeModal() {
+    signinModal.style.display = "none";
+    signupModal.style.display = "none";
+}
