08768a07a437395b59fae72d6213c3a1a0d5d4a6
Sehbaz Rafik <sehbazm@gmail.com>
2021-02-15 08:29:57 +0000

    create post modal created with css

--- scripts/common.js
+++ scripts/common.js
@@ -11,9 +11,16 @@ function opensignupModal() {
 function closeModal() {
     signinModal.style.display = "none";
     signupModal.style.display = "none";
+    createModal.style.display = "none";
 }
 
 function redirect() {
     signinModal.style.display = "none";
     signupModal.style.display = "block";
+}
+
+var createModal = document.getElementById("createModal");
+function opencreateModal() {
+    createModal.style.display = "block";
+  
 }
