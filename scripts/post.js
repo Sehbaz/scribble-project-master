dc7875b4d35b1cf8f4712e685d768c81af0245b7
Sehbaz Rafik <sehbazm@gmail.com>
2021-02-15 14:52:46 +0000

    post css added

--- scripts/post.js
+++ scripts/post.js
@@ -4,22 +4,33 @@ var postContent = document.getElementById("postContent");
 
 var i = 0;
 
-
-function edit() {
-    if (i==0) {
-    console.log(editPost);
-    postContent.contentEditable = true;
-    postContent.style.border = "1px red solid";
-        document.getElementById("editPost").innerText = "save";
-        i++;
-    } else {
-        postContent.contentEditable = false;
-        postContent.style.border = "0px red solid";
-        document.getElementById("editPost").innerText = "edit";
-        i--;
+ 
+    
+    function edit() {
+        if (i==0) {
+        console.log(editPost);
+            postContent.contentEditable = true;
+            postTitle.contentEditable = true;
+            postContent.style.border = "1px #ff0a54 solid";
+            postTitle.style.border = "1px #ff0a54 solid";
+            document.getElementById("editPost").innerText = "save";
+    
+            i++;
+        } else {
+            postContent.contentEditable = false;
+            postTitle.contentEditable = false;
+            postContent.style.border = "0px red solid";
+            postTitle.style.border = "0px #ff0a54 solid";
+            document.getElementById("editPost").innerText = "edit";
+            document.getElementById("update-title").innerText = "UPDATED :";
+            document.getElementById("update-content").innerText = "UPDATED :";
+            i--;
+        }
+        
     }
     
-}
+
+
 
 var j = 0;
 
@@ -29,9 +40,11 @@ function addLike() {
 }
 
 function addComment() {
+   
     var node = document.createElement("LI");
     var t = document.getElementById("comment").value;
-    if (t!="") {
+    if (t != "") {
+        document.getElementById("myList").style.display = "block";
         var textnode = document.createTextNode(t);
     node.appendChild(textnode);
     document.getElementById("myList").appendChild(node);
